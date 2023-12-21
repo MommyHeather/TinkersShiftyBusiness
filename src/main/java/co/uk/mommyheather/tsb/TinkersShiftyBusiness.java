20b75db6cdbb26ea0135ec2623a3638b839fb58e
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-12-21 19:25:37 +0000

    Empty "Mod" so it shows in modlist

---
+++ src/main/java/co/uk/mommyheather/tsb/TinkersShiftyBusiness.java
@@ -0,0 +1,19 @@
+package co.uk.mommyheather.tsb;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+
+
+@Mod(modid = TinkersShiftyBusiness.MODID, name = TinkersShiftyBusiness.NAME, version = TinkersShiftyBusiness.VERSION)
+@EventBusSubscriber
+public class TinkersShiftyBusiness
+{
+
+    
+    public static final String MODID = "tsb";
+    public static final String NAME = "Tinkers' Shifty Business";
+    public static final String VERSION = "1.0";
+
+
+}
